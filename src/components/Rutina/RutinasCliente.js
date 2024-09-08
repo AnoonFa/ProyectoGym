7302b4de5534443c11967e95bdd457e8b6e6ecc9
ElPinasXD <160042562+ElPinasXD@users.noreply.github.com>
2024-09-08 14:33:08 -0500

    Se modifico la interfaz grafica de Rutinas para Cliente, falta imagenes y video

--- src/components/Rutina/RutinasCliente.js
+++ src/components/Rutina/RutinasCliente.js
@@ -4,36 +4,27 @@ import { useNavigate } from 'react-router-dom';
 import './RutinaAdminIndex.css';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import EjercicioModal from './Modal/EjercicioModal';
 
 const RutinasCliente = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
   const [rutina, setRutina] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedEjercicio, setSelectedEjercicio] = useState(null);
 
   useEffect(() => {
-    console.log('useEffect se ejecuta');
-    console.log('Valor de user en useEffect:', user);
     if (user && user.id) {
-      console.log('Usuario tiene ID:', user.id);
       fetch(`http://localhost:3001/client/${user.id}`)
-        .then(response => {
-          console.log('Response status:', response.status);
-          return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-          try {
-            const rutinasParsed = data.rutinas ? JSON.parse(data.rutinas) : [];
-            console.log('Rutinas parseadas:', rutinasParsed);
-            setRutina(rutinasParsed);
-          } catch (error) {
-            console.error('Error al parsear rutinas:', error);
-          }
+          console.log("Rutina obtenida:", data); // Agregar log para verificar los datos
+          const rutinasParsed = data.rutinas ? JSON.parse(data.rutinas) : [];
+          setRutina(rutinasParsed);
         })
         .catch(error => {
           console.error('Error al obtener la rutina del cliente:', error);
         });
-    } else {
-      console.log('Usuario no está definido o no tiene ID');
     }
   }, [user]);
 
@@ -41,35 +32,46 @@ const RutinasCliente = () => {
     navigate('/ClienteIndex');
   };
 
+  const openModal = (ejercicio) => {
+    setSelectedEjercicio(ejercicio);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedEjercicio(null);
+  };
+
   return (
     <>
       <Header />
       <div className="container">
-      <h1 className="title">Mi rutina</h1>
-      <div className="grid">
-        {rutina.length > 0 ? (
-          rutina.map((ejercicio, index) => (
-            <div key={index} className="card">
-              <img
-                aria-hidden="true"
-                alt={ejercicio.nombre}
-                src={`https://openui.fly.dev/openui/150x150.svg?text=${encodeURIComponent(ejercicio.nombre)}`}
-                className="card-image"
-              />
-              <h2 className="card-title">{ejercicio.nombre}</h2>
-              <p className="card-detail">Series: {ejercicio.series}</p>
-              {ejercicio.series !== 'Máximo de repeticiones' && (
-                <p className="card-detail">Repeticiones: {ejercicio.repeticiones || '-'}</p>
-              )}
-              <p className="card-detail">Descanso (s): {ejercicio.descanso}</p>
-            </div>
-          ))
-        ) : (
-          <p className='no-exercises'>No se encontraron ejercicios en tu rutina.</p>
-        )}
+        <h1 className="title">Mi rutina</h1>
+        <div className="exercise-grid">
+          {rutina.length > 0 ? (
+            rutina.map((ejercicio, index) => (
+              <div key={index} className="exercise-card" onClick={() => openModal(ejercicio)}>
+                <img
+                  src={`ProyectoGym/src/assets/images/Ejercicios/${ejercicio.nombre.replace(/\s+/g, '_').toLowerCase()}.jpg`}
+                  alt={ejercicio.nombre}
+                  className="exercise-image"
+                />
+                <h2 className="exercise-title">{ejercicio.nombre}</h2>
+              </div>
+            ))
+          ) : (
+            <p className='no-exercises'>No se encontraron ejercicios en tu rutina.</p>
+          )}
+        </div>
+        <button className="retuarn-btn-cliente" onClick={handleVolver}>Volver</button>
       </div>
-      <button className="return-btn" onClick={handleVolver}>Volver</button>
-    </div>
+      {modalOpen && selectedEjercicio && (
+        <EjercicioModal
+          ejercicio={selectedEjercicio}
+          tipoCuerpo={user.tipoCuerpo}  // Pasa el tipo de cuerpo del usuario
+          onClose={closeModal}
+        />
+      )}
       <Footer />
     </>
   );
