62974e70cb7ca1f5267a6edf2a8461c6a72de1d1
3scanor5069 <160270567+3scanor5069@users.noreply.github.com>
2024-11-05 00:01:29 -0500

    Update MisPlanes.js

--- src/pages/Planes/MisPlanes.js
+++ src/pages/Planes/MisPlanes.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useAuth } from '../../context/RoleContext'; // Para obtener el usuario autenticado
+import { useAuth } from '../../context/RoleContext';
+
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import './MisPlanes.css'; // Asegúrate de tener un archivo CSS para estilos
-import Relleno from '../../components/Relleno/Relleno';
+import './MisPlanes.css';
+import { format, add } from 'date-fns';
 
 const MisPlanes = () => {
-  const { user } = useAuth(); // Obtener el usuario autenticado
+  const { user } = useAuth();
   const [planes, setPlanes] = useState([]);
 
   useEffect(() => {
@@ -26,11 +27,32 @@ const MisPlanes = () => {
     };
 
     if (user) {
-      fetchUserPlans(); // Solo obtenemos los planes si el usuario está autenticado
+      fetchUserPlans();
     }
   }, [user]);
 
-  // Función para cargar la imagen local con require basado en el plan
+  // Calcula la fecha de expiración del plan
+  const calculateEndDate = (startDate, duration) => {
+    const parsedStartDate = new Date(startDate);
+    if (isNaN(parsedStartDate)) {
+      console.error("Fecha de inicio inválida:", startDate);
+      return 'Fecha de expiración desconocida';
+    }
+
+    switch (duration) {
+      case '1 mes':
+        return format(add(parsedStartDate, { months: 1 }), 'dd/MM/yyyy');
+      case '3 meses':
+        return format(add(parsedStartDate, { months: 3 }), 'dd/MM/yyyy');
+      case '6 meses':
+        return format(add(parsedStartDate, { months: 6 }), 'dd/MM/yyyy');
+      case '1 año':
+        return format(add(parsedStartDate, { years: 1 }), 'dd/MM/yyyy');
+      default:
+        return 'Fecha de expiración desconocida';
+    }
+  };
+
   const loadImage = (planId) => {
     try {
       switch (planId) {
@@ -43,30 +65,29 @@ const MisPlanes = () => {
         case 'p4':
           return require('../../assets/images/planes4.jpg');
         default:
-          return require('../../assets/images/planes5.jpg'); // Imagen por defecto
+          return require('../../assets/images/planes5.jpg');
       }
     } catch (error) {
       console.error('Error cargando la imagen:', error);
-      return require('../../assets/images/planes5.jpg'); // Imagen predeterminada en caso de error
+      return require('../../assets/images/planes5.jpg');
     }
   };
 
   return (
     <div>
       <Header />
-      <Relleno/>
       <div className="mis-planes-container">
         <h1>Mis Planes</h1>
         {planes.length > 0 ? (
           <div className="mis-planes-grid">
             {planes.map((plan) => (
               <div key={plan.id} className="plan-card">
-                {/* Cargar la imagen con require basado en el id del plan */}
                 <img src={loadImage(plan.id)} alt={plan.name} className="plan-image" />
                 <div className="plan-info">
                   <h2>{plan.name}</h2>
                   <p>{plan.description}</p>
                   <p className="price"><strong>Precio:</strong> ${plan.price}</p>
+                  <p><strong>Fecha de expiración:</strong> {calculateEndDate(plan.startDate, plan.duration)}</p>
                 </div>
               </div>
             ))}
